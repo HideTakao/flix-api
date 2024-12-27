c352048a870c86565203bad29242c8eca07d2d81
Angelo Takao <shidehister@gmail.com>
2024-12-27 00:24:16 -0300

    Criação do app movies

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView,GenreRetriveUpdateDestroyView
 from actors.views import ActorCreatListView, ActorRetriveUpdateDestroyView
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 
 
 
@@ -9,6 +10,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres', GenreCreateListView.as_view(), name='genre-criate-list'),
     path('genres/<int:pk>', GenreRetriveUpdateDestroyView.as_view(), name='gente-detail-view'),
+
     path('actors', ActorCreatListView.as_view(), name='actor-criate-list'),
     path('actors/<int:pk>', ActorRetriveUpdateDestroyView.as_view(), name='actor-detail-view'),
+
+    path('movies', MovieCreateListView.as_view(), name='movie-criate-list'),
+    path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
 ]
--- movies/models.py
+++ movies/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from genres.models import Genre
+from actors.models import Actor
 
 class Movie(models.Model):
     title = models.CharField(max_length=500)
@@ -8,3 +9,9 @@ class Movie(models.Model):
         on_delete=models.PROTECT,
         related_name='movies'
     )
+    release_date = models.DateField(null=True, blank= True)
+    actors = models.ManyToManyField(Actor, related_name= 'movies')
+    resume = models.TextField(null= True, blank= True)
+
+    def __str__(self):
+        return self.title
