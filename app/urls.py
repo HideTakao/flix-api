eafef215095e49e2d2b997e3355381e8f12be1aa
Angelo Takao <shidehister@gmail.com>
2024-12-27 22:52:15 -0300

    criação do app reviews

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from genres.views import GenreCreateListView,GenreRetriveUpdateDestroyView
 from actors.views import ActorCreatListView, ActorRetriveUpdateDestroyView
 from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
-
+from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 
 urlpatterns = [
@@ -16,4 +16,7 @@ urlpatterns = [
 
     path('movies', MovieCreateListView.as_view(), name='movie-criate-list'),
     path('movies/<int:pk>', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
+
+    path('reviews', ReviewCreateListView.as_view(), name='reviews-create-view'),
+    path('reviews/<int:pk>', ReviewRetrieveUpdateDestroyView.as_view(), name='reviews-detail-view'),
 ]
